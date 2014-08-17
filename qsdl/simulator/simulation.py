b02ef98f2aad1cac5389453f13c0b90ad906d641
Teemu Pääkkönen <teemu.paakkonen@iki.fi>
2014-08-17 18:14:29 +0300

    Amend simulation.py to conform with style guide

--- qsdl/simulator/simulation.py
+++ qsdl/simulator/simulation.py
@@ -42,9 +42,9 @@ def pack_callback_arguments( callback ):
 
 class SimulationState(object):
     '''
-    objects of this class represent a simulation state 
+    objects of this class represent a simulation state
     '''
-    def __init__(self, sessionId, queryIndex, totalRank, cumulatedGain, 
+    def __init__(self, sessionId, queryIndex, totalRank, cumulatedGain,
                  cumulatedCost, nextTransition, prevAction, iteration,
                  currentQueryRank, gains):
         self.transitionsConsidered = []
@@ -60,20 +60,20 @@ class SimulationState(object):
         self.iteration = iteration
         self.currentQueryRank = currentQueryRank
         self.ready = False # Ready to run next action?
-        
+
     def add_transition_probability(self, pyxbTransition, probabilityValue):
         self.transitionProbabilities[ pyxbTransition.target ] = probabilityValue
-        
+
     def add_transition_considered(self, pyxbTransition):
         self.transitionsConsidered.append( pyxbTransition.target )
-        
+
     def set_ready(self):
         self.ready = True
-        
+
     def get_copy(self):
-        return SimulationState( self.sessionId, self.queryIndex, self.totalRank, 
+        return SimulationState( self.sessionId, self.queryIndex, self.totalRank,
                                 self.cumulatedGain, self.cumulatedCost, None,
-                                self.nextTransition.target, self.iteration, 
+                                self.nextTransition.target, self.iteration,
                                 self.currentQueryRank, self.gains.copy() )
 
     def get_derived_gains(self):
@@ -81,7 +81,7 @@ class SimulationState(object):
 
     @staticmethod
     def get_field_order( includeGains = True ):
-        order = [ 'sessionId', 'iteration', 'prevAction', 'queryIndex', 'totalRank', 
+        order = [ 'sessionId', 'iteration', 'prevAction', 'queryIndex', 'totalRank',
                 'currentQueryRank', 'cumulatedGain', 'cumulatedCost', 'transitionsConsidered',
                 'transitionProbabilities' ]
         if includeGains:
@@ -100,9 +100,9 @@ class Simulation(Observable):
         return repr( self.history )
 
     def __init__(self, simDesc, config, reader, iteration, runId):
-        
+
         Observable.__init__(self)
-        
+
         self.simDesc = simDesc
         self.config = config
         self.reader = reader
@@ -110,9 +110,9 @@ class Simulation(Observable):
         self.runId = runId
         self.history = []
         self.seenDocuments = {}
-        
+
         sessionId = reader['get_session_id']()
-        
+
         self.currentState = SimulationState( sessionId, -1, 0, 0, 0,
                         create_initial_transition( simDesc.initialActionId ), None,
                         self.iteration, 0,
@@ -120,11 +120,11 @@ class Simulation(Observable):
         self.currentState.set_ready()
         self.conditionCallbacks = self.config.get_condition_callbacks()
         self.v_print = self.config.get_verbose_writer()
-        
+
         self.transitionStatistics = {}
         self.transitionStatistics[ 'transitionsFrom' ] = {}
         self.transitionStatistics[ 'totalsFrom' ] = {}
-        
+
     def record_transition_statistics( self, fromActionId, toActionId ):
         if fromActionId == None:
             return
@@ -147,7 +147,7 @@ class Simulation(Observable):
                 return lastState
             lastState = state
         return None
-    
+
     def get_first_state_at_cost(self, cost):
         lastState = None
         for state in self.history:
@@ -184,7 +184,7 @@ class Simulation(Observable):
 
     def get_current_total_rank(self):
         return self.currentState.totalRank
-    
+
     def get_current_query_rank(self):
         return self.currentState.currentQueryRank
 
@@ -205,7 +205,7 @@ class Simulation(Observable):
 
     def get_current_document_relevance_level(self):
         return self.reader[ 'get_current_relevance_level' ]( self.get_current_document_id() )
-    
+
     def get_current_results_length(self):
         return self.reader[ 'get_current_results_length' ]()
 
@@ -239,7 +239,7 @@ class Simulation(Observable):
                 args = pack_callback_arguments( gains[ gainId ] )
                 self.currentState.gains[ gainId ] += callback( self, **args )
             except KeyError:
-                raise CallbackError( 
+                raise CallbackError(
                     'Derived gain callback \'%s\' not found. Check callback mapping. Available callbacks: %s' % \
                     ( cbFunctionName, repr(callbackFunctions.keys())))
 
@@ -250,12 +250,12 @@ class Simulation(Observable):
         # FIXME: type can currently be either from qsdl xsd or config xsd
         if type( valueToCheck ) == qsdl.probability_value_direct:
             decayedValue = valueToCheck + decay
-            
+
             # Notify observers that a probability value has been calculated
             self.notifyObservers( ProbabilityCalculated( self.currentState, transition, decayedValue ) )
-            
+
             if decayedValue + H1.cumulatingProbability >= H1.probVal:
-                self.v_print( lambda : '   %s >= remaining probability %g. Choosing action.' % (decayedValue, H1.probVal - H1.cumulatingProbability) ) 
+                self.v_print( lambda : '   %s >= remaining probability %g. Choosing action.' % (decayedValue, H1.probVal - H1.cumulatingProbability) )
                 return transition
             else:
                 self.v_print( lambda : '   %s < remaining probability %g. Skipping action.' % (decayedValue, H1.probVal - H1.cumulatingProbability) )
@@ -305,39 +305,39 @@ class Simulation(Observable):
                         continue
                 if skipToNextTransition:
                     return None
-                    
+
                 # None of the if-else-if conditions was met. Move to the else block.
                 nextTarget = self.get_transition_by_probability( probability.else_.value_, decay, H1, transition )
                 if not nextTarget == None:
                     return nextTarget
                 else:
                     return None
-        
+
 
     def get_next_transition( self ):
-        
+
         class H1 : pass # Helper class for containing method-local vars
-        
+
         H1.probVal = generate_random_probability()
         H1.cumulatingProbability = 0.0
         H1.calculateTarget = None
-        transitionSource = self.simDesc.get_transition_source_for_action_id( 
+        transitionSource = self.simDesc.get_transition_source_for_action_id(
                                             self.get_last_transition().target )
         decayFunction = transitionSource.decayFunction
         for transition in transitionSource.targets:
-            
+
             # Notify observers of transition considering beginning
             self.notifyObservers( TransitionConsiderBegin( self.currentState, transition ) )
-            
+
             decay = transition.decayFunction( decayFunction, self )
             probability = self.simDesc.get_probability_for_probability_id( transition.probability )
             self.v_print( lambda : 'Considering target action: %s, probability %s, decay %g' % (transition.target, probability.id, decay) )
-            
+
             nextTarget = self.get_transition_by_probability_definition( probability, decay, H1, transition )
-            
+
             # Notify observers of transition considering ending
             self.notifyObservers( TransitionConsiderEnd( self.currentState, transition ) )
-            
+
             if nextTarget != None:
                 # Notify observers of a transition being accepted
                 self.notifyObservers( TransitionAccepted( self.currentState, transition ) )
@@ -346,16 +346,16 @@ class Simulation(Observable):
                 # Notify observers of a transition being rejected
                 self.notifyObservers( TransitionRejected( self.currentState, transition ) )
                 continue
-            
+
         # Didn't hit any transition.
         # Use calculated target as fallback.
         if H1.calculateTarget == None:
             raise TransitionError.TransitionError( 'Unable to determine next transition for action %s. Please check simulation description.' % self.currentState.prevAction )
-        
+
         # Notify observers of the fallback transition being accepted
         self.notifyObservers( TransitionAccepted( self.currentState, H1.calculateTarget ) )
         return H1.calculateTarget
-    
+
     def advance(self):
         '''
         Advances the simulation one step:
@@ -364,7 +364,7 @@ class Simulation(Observable):
         3. Creates a new simulation state object for the next state
         4. Runs the triggers associated with next transition's target action
         5. Calculates gains and costs
-        Returns True if the next transition is not final (simulation should continue). 
+        Returns True if the next transition is not final (simulation should continue).
         Otherwise False.
         '''
 
@@ -375,7 +375,7 @@ class Simulation(Observable):
             if self.currentState.nextTransition == None:
                 self.currentState.nextTransition = self.get_next_transition()
         except UnknownDocumentError.UnknownDocumentError:
-            # No document for current rank was found. Check if 
+            # No document for current rank was found. Check if
             # the reason is because we ran out of results.
             if self.get_current_rank() > self.get_current_results_length():
                 return False
@@ -388,60 +388,60 @@ class Simulation(Observable):
         if hasattr( self.currentState.nextTransition, 'target' ) and (
             self.currentState.nextTransition.target != None):
             nextAction = self.simDesc.get_action_by_id( self.currentState.nextTransition.target )
-        
+
         # Notify observers of current transition beginning
         self.notifyObservers( TransitionBegin( self.currentState ) )
-        
+
         # Currently, transitions are instantaneous
-        
-        
+
+
         # Notify observers of current transition ending
         self.notifyObservers( TransitionEnd( self.currentState ) )
-        
+
         self.record_transition_statistics( self.currentState.prevAction, self.currentState.nextTransition.target )
         self.history.append( self.currentState )
         self.currentState = self.currentState.get_copy()
-        
+
         if nextAction != None:
-            
+
             # Notify observers of current action beginning
             self.notifyObservers( ActionBegin( self.currentState ) )
-            
+
             triggers.run_triggers( nextAction, self )
             if hasattr( nextAction, 'cost' ) and nextAction.cost != None:
                 cost = self.simDesc.get_cost_callback_by_id( nextAction.cost )
                 if hasattr( cost, 'callback' ) and cost.callback != None:
                     try:
                         self.currentState.cumulatedCost += \
-                                self.config.get_cost_callbacks()[ cost.callback.name ]( 
+                                self.config.get_cost_callbacks()[ cost.callback.name ](
                                     self, **cost.callbackArgs )
                     except KeyError:
-                        raise CallbackError( 
+                        raise CallbackError(
                             'Cost callback \'%s\' not found. Check callback mapping. Available callbacks: %s' \
                             % ( cost.callback.name, repr(self.config.get_cost_callbacks().keys())))
                 else:
                     self.currentState.cumulatedCost += cost.value_
-                
+
             if hasattr( nextAction, 'gain' ) and nextAction.gain != None:
                 gain = self.simDesc.get_gain_callback_by_id( nextAction.gain )
                 if hasattr( gain, 'callback' ) and gain.callback != None:
                     try:
-                        self.currentState.cumulatedGain += self.config.get_gain_callbacks()[ gain.callback.name ]( 
+                        self.currentState.cumulatedGain += self.config.get_gain_callbacks()[ gain.callback.name ](
                                 self, **gain.callbackArgs )
                     except KeyError:
-                        raise CallbackError( 
+                        raise CallbackError(
                             'Gain callback \'%s\' not found. Check callback mapping. Available callbacks: %s' \
                             % ( gain.callback.name, repr(self.config.get_gain_callbacks().keys())))
                 else:
                     self.currentState.cumulatedGain += gain.value_
-                    
+
                 self.calculate_derived_gains()
-                
+
             # Notify observers of current action ending
             self.notifyObservers( ActionEnd( self.currentState ) )
 
         if nextAction.final:
             return False
-         
+
         return True
-    
+
