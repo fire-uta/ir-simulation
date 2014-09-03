042f21e024d805be657d06f69d3c865e947879c9
Teemu Pääkkönen <teemu@flockler.com>
2014-09-03 15:19:54 +0300

    Catch validation errors for simulation description and show error details

--- irsim.py
+++ irsim.py
@@ -23,19 +23,25 @@ except ValidationError as e:
     sys.stderr.write(e.details() + '\n')
     sys.exit(1)
 
-for runId in confDesc.get_run_id_iterator():
-    figures.set_run_id(runId)
-    for simulationIterations in simulationRunner.run_sessions( confDesc, runId ):
-        confDesc.get_output_writer( simulationIterations, confDesc.get_random_seed(), configName, runId )()
+try:
+    for runId in confDesc.get_run_id_iterator():
+        figures.set_run_id(runId)
+        for simulationIterations in simulationRunner.run_sessions( confDesc, runId ):
+            confDesc.get_output_writer( simulationIterations, confDesc.get_random_seed(), configName, runId )()
 
-        costIncrement = 10
-        sessid = str(simulationIterations[0].get_session_id())
+            costIncrement = 10
+            sessid = str(simulationIterations[0].get_session_id())
 
-        #FIXME: figures module should derive filenames from runId
-        figures.plotGainsAtRank( simulationIterations )
-        figures.plotGainsAtCost(simulationIterations, costIncrement)
-        figures.plotDerivedGains(simulationIterations, confDesc.get_derived_gains_dict( sessid ).iterkeys(), costIncrement)
-        figures.plotCostsAtRank(simulationIterations)
+            #FIXME: figures module should derive filenames from runId
+            figures.plotGainsAtRank( simulationIterations )
+            figures.plotGainsAtCost(simulationIterations, costIncrement)
+            figures.plotDerivedGains(simulationIterations, confDesc.get_derived_gains_dict( sessid ).iterkeys(), costIncrement)
+            figures.plotCostsAtRank(simulationIterations)
 
-        figures.plotCustomFigures(simulationIterations, confDesc.get_custom_figures_dict(sessid))
+            figures.plotCustomFigures(simulationIterations, confDesc.get_custom_figures_dict(sessid))
+
+except ValidationError as e:
+    sys.stderr.write( 'ERROR: Invalid content found in simulation description.\n' )
+    sys.stderr.write(e.details() + '\n')
+    sys.exit(1)
 
