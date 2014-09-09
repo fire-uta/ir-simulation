8069f1f02966dc9e15b966cff5ed905a6b997627
Teemu Pääkkönen <teemu@flockler.com>
2014-09-09 17:04:08 +0300

    Bump version number

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 # -*- coding: latin-1 -*-
 from distutils.core import setup
 setup(name='irsim',
-      version='0.1',
+      version='0.1.2',
       py_modules=['irsim', 'callbackLoader', 'stats', 'figures'],
       packages=['qsdl', 'qsdl.parser', 'qsdl.simulator', 'qsdl.simulator.errors'],
-      data_files=[ ( 'example-config', 
-                     [ 
-                      'example-config/config.xml', 
+      data_files=[ ( 'example-config',
+                     [
+                      'example-config/config.xml',
                       'example-config/config-0.1.xsd',
                       'example-config/simulation.xml',
-                      'example-config/qsdl-0.1.xsd', 
+                      'example-config/qsdl-0.1.xsd',
                       'example-config/customConditionCallbacks.py',
-                      'example-config/customCostCallbacks.py', 
+                      'example-config/customCostCallbacks.py',
                       'example-config/customDecayCallbacks.py',
                       'example-config/customDerivedGainsCallbacks.py',
                       'example-config/customGainCallbacks.py',
-                      'example-config/customTriggerCallbacks.py' 
+                      'example-config/customTriggerCallbacks.py'
                       ] ),
                   ( '', ['irsim'] ) ],
       url='http://www.github.com/fire-uta/ir-simulation/',
