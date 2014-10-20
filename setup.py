23439717435e05622691903ab3402b70c16b7ae3
Teemu Pääkkönen <teemu@flockler.com>
2014-10-20 22:22:38 +0300

    Version bump to 0.1.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: latin-1 -*-
 from distutils.core import setup
 setup(name='irsim',
-      version='0.1.5',
+      version='0.1.6',
       py_modules=['irsim', 'callbackLoader', 'stats', 'figures'],
       packages=['qsdl', 'qsdl.parser', 'qsdl.simulator', 'qsdl.simulator.errors'],
       data_files=[ ( 'example-config',
