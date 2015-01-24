8316667c8dd3aad7f3af1aecbdd8f866f04cf7b9
Teemu Pääkkönen <teemu@flockler.com>
2015-01-24 21:03:34 +0200

    Raise an error on missing callback files

--- callbackLoader.py
+++ callbackLoader.py
@@ -9,23 +9,27 @@ import imp
 import sys
 import os
 
+from qsdl.simulator.errors.ConfigurationInvalidError import ConfigurationInvalidError
+
 def get_callback_module( name ):
-    
+
     # Already loaded?
     try:
         return sys.modules[name]
     except KeyError:
         pass
-    
+
     fp = pathname = description = None
-    
+
     try:
         fp, pathname, description = imp.find_module(name, [os.getcwdu()])
         return imp.load_module(name, fp, pathname, description)
+    except ImportError as e:
+        raise ConfigurationInvalidError("Callback file not found: %s" % name)
     except:
         return None
     finally:
         if fp:
             fp.close()
-            
-            
+
+
