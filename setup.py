295716ef02492d82ccdf5fdaf3cde742151dfcdc
Teemu Pääkkönen <teemu@flockler.com>
2016-08-22 22:38:43 +0300

    version bump

--- setup.py
+++ setup.py
@@ -1,23 +1,12 @@
 # -*- coding: latin-1 -*-
 from distutils.core import setup
 setup(name='irsim',
-      version='0.1.23',
+      version='0.1.25',
       py_modules=['irsim', 'callbackLoader', 'stats', 'figures'],
-      packages=['qsdl', 'qsdl.parser', 'qsdl.simulator', 'qsdl.simulator.errors'],
-      data_files=[ ( 'example-config',
-                     [
-                      'example-config/config.xml',
-                      'example-config/config-0.1.xsd',
-                      'example-config/simulation.xml',
-                      'example-config/qsdl-0.1.xsd',
-                      'example-config/customConditionCallbacks.py',
-                      'example-config/customCostCallbacks.py',
-                      'example-config/customDecayCallbacks.py',
-                      'example-config/customDerivedGainsCallbacks.py',
-                      'example-config/customGainCallbacks.py',
-                      'example-config/customTriggerCallbacks.py'
-                      ] ),
-                  ( '', ['irsim'] ) ],
+      packages=['qsdl', 'qsdl.parser', 'qsdl.simulator', 'qsdl.simulator.errors', 'example-config'],
+      package_data={'example-config': ['*.xml', '*.xsd']},
+      data_files=[('', ['requirements.txt'])],
+      scripts=['irsim'],
       url='http://www.github.com/fire-uta/ir-simulation/',
       author='Teemu Pääkkönen, University of Tampere',
       author_email='teemu.paakkonen@uta.fi'
